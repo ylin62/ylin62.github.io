66b57d302393d727dc479f304204e097d197315a
Yu Lin <yu.saturn@gmail.com>
2025-11-25 21:18:49 -0800

    update resume

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ca1e09733478bd688e59e0354267a787.js"
+  "/precache-manifest.bd116faaa6f8775f71c71887d7254d27.js"
 );
 
 self.addEventListener('message', (event) => {
